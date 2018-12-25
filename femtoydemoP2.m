067bcfac5434536226d29958d18c63b236a738f7
Han <hanyin15@163.com>
2018-12-25 11:07:21 +0800

    back up the calculate stuff into T.P2

--- femtoydemoP2.m
+++ femtoydemoP2.m
@@ -13,35 +13,35 @@ Py = [zeros(12,1), zeros(12,1), ones(12,1), zeros(12,1), px, 2*py];
 
 nx = 2; ny = 2;
 T = RecMesh(nx, ny, 1, 1, 0, 0);
-T.Nodes = [T.Nodes; zeros(T.Ne, 2)];
+T.P2.Nodes = [T.Nodes; zeros(T.Ne, 2)];
 for i = 1:T.Ne
     cord = T.Nodes(T.Edge(i,:),:);
-    T.Nodes(i+T.N, :) = mean(cord, 1);
+    T.P2.Nodes(i+T.N, :) = mean(cord, 1);
 end
-T.TC = [T.Tri, T.TrEg+T.N];
-T.TP = [T.TC(:,1) T.TC(:,4) T.TC(:,6);
-    T.TC(:,4) T.TC(:,2) T.TC(:,5);
-    T.TC(:,5) T.TC(:,3) T.TC(:,6);
-    T.TC(:,4) T.TC(:,5) T.TC(:,6)];
+T.P2.TC = [T.Tri, T.TrEg+T.N];
+T.P2.TP = [T.P2.TC(:,1) T.P2.TC(:,4) T.P2.TC(:,6);
+    T.P2.TC(:,4) T.P2.TC(:,2) T.P2.TC(:,5);
+    T.P2.TC(:,5) T.P2.TC(:,3) T.P2.TC(:,6);
+    T.P2.TC(:,4) T.P2.TC(:,5) T.P2.TC(:,6)];
 Nf = (2*nx+1)*(2*ny+1) - (4*nx+4*ny);
-T.FNodePtrs = zeros(Nf, 1);
-T.CNodePtrs = zeros(4*nx+4*ny ,1);
-T.NodePtrs = zeros((2*nx+1)*(2*ny+1), 1);
-T.NodeFlag = zeros((2*nx+1)*(2*ny+1), 1);
+T.P2.FNodePtrs = zeros(Nf, 1);
+T.P2.CNodePtrs = zeros(4*nx+4*ny ,1);
+T.P2.NodePtrs = zeros((2*nx+1)*(2*ny+1), 1);
+T.P2.NodeFlag = zeros((2*nx+1)*(2*ny+1), 1);
 indf = 0; indc = 0;
 for i = 1:(2*nx+1)*(2*ny+1)
     flag = 0;
-    if T.Nodes(i, 2) == 0; T.NodeFlag(i) = 1; flag = 1;
-    elseif T.Nodes(i, 1) == 1; T.NodeFlag(i) = 2; flag = 1;
-    elseif T.Nodes(i, 2) == 1; T.NodeFlag(i) = 3; flag = 1;
-    elseif T.Nodes(i, 1) == 0; T.NodeFlag(i) = 4; flag = 1;
+    if T.P2.Nodes(i, 2) == 0; T.P2.NodeFlag(i) = 1; flag = 1;
+    elseif T.P2.Nodes(i, 1) == 1; T.P2.NodeFlag(i) = 2; flag = 1;
+    elseif T.P2.Nodes(i, 2) == 1; T.P2.NodeFlag(i) = 3; flag = 1;
+    elseif T.P2.Nodes(i, 1) == 0; T.P2.NodeFlag(i) = 4; flag = 1;
     end
-    if flag; indc = indc+1; T.CNodePtrs(indc) = i;
-    else; indf = indf+1; T.FNodePtrs(indf) = i;
+    if flag; indc = indc+1; T.P2.CNodePtrs(indc) = i;
+    else; indf = indf+1; T.P2.FNodePtrs(indf) = i;
     end
 end
-T.NodePtrs(T.FNodePtrs) = 1:indf;
-T.NodePtrs(T.CNodePtrs) = 1:indc;
+T.P2.NodePtrs(T.P2.FNodePtrs) = 1:indf;
+T.P2.NodePtrs(T.P2.CNodePtrs) = 1:indc;
 
 
 Ibasis = zeros(5,5);
@@ -60,17 +60,17 @@ C = inv(M); % Coefficients of basis funcitons
 K = zeros(Nf, Nf);
 for i = 1:T.Nt
     j = T.Tri(i, :);
-    cord = T.Nodes(j,:);
+    cord = T.P2.Nodes(j,:);
     J = (cord(2:3,:) - repmat(cord(1,:),2,1))';
     A = abs(det(J));
     J0 = inv(J);
-    j = T.TC(i, :);
-    jp = T.NodePtrs(j);
+    j = T.P2.TC(i, :);
+    jp = T.P2.NodePtrs(j);
     for s = 1:6
-        if T.NodeFlag(j(s)) == 0
+        if T.P2.NodeFlag(j(s)) == 0
             c1 = C(:, s);
             for r = 1:s
-                if T.NodeFlag(j(r)) == 0
+                if T.P2.NodeFlag(j(r)) == 0
                     c2 = C(:, r);
                     V = [Px*c1 Py*c1 Px*c2 Py*c2]; % Value of derivatives of basis functions
                     I = w*((V(:, 1:2)*J0(:,1)).*(V(:, 3:4)*J0(:,1))+(V(:, 1:2)*J0(:,2)).*(V(:, 3:4)*J0(:,2)))/2;
@@ -87,27 +87,27 @@ g = @(x) 1+0.3*sin(pi*x);
 F = zeros(Nf, 1);
 for i = 1:T.Nt
     j = T.Tri(i, :);
-    cord = T.Nodes(j,:);
+    cord = T.P2.Nodes(j,:);
     J = (cord(2:3,:) - repmat(cord(1,:),2,1))';
     A = abs(det(J));
     J0 = inv(J);
-    j = T.TC(i, :);
-    jp = T.NodePtrs(j);
+    j = T.P2.TC(i, :);
+    jp = T.P2.NodePtrs(j);
     for s = 1:6
-        if T.NodeFlag(j(s)) == 0
+        if T.P2.NodeFlag(j(s)) == 0
             c1 = C(:, s);
             for r = 1:6
-                if T.NodeFlag(j(r)) == 3
+                if T.P2.NodeFlag(j(r)) == 3
                     c2 = C(:, r);
                     V = [Px*c1 Py*c1 Px*c2 Py*c2]; % Value of derivatives of basis functions
                     I = w*((V(:, 1:2)*J0(:,1)).*(V(:, 3:4)*J0(:,1))+(V(:, 1:2)*J0(:,2)).*(V(:, 3:4)*J0(:,2)))/2;
-                    F(jp(s)) = F(jp(s)) - I*A*g(T.Nodes(j(r),1));
+                    F(jp(s)) = F(jp(s)) - I*A*g(T.P2.Nodes(j(r),1));
                 end
-                if T.NodeFlag(j(r)) == 2 || T.NodeFlag(j(r)) == 4
+                if T.P2.NodeFlag(j(r)) == 2 || T.P2.NodeFlag(j(r)) == 4
                     c2 = C(:, r);
                     V = [Px*c1 Py*c1 Px*c2 Py*c2]; % Value of derivatives of basis functions
                     I = w*((V(:, 1:2)*J0(:,1)).*(V(:, 3:4)*J0(:,1))+(V(:, 1:2)*J0(:,2)).*(V(:, 3:4)*J0(:,2)))/2;
-                    F(jp(s)) = F(jp(s)) - I*A*(T.Nodes(j(r),2));
+                    F(jp(s)) = F(jp(s)) - I*A*(T.P2.Nodes(j(r),2));
                 end
             end
         end
@@ -116,10 +116,10 @@ end
 
 U = K\F;
 Z = zeros(T.N, 1);
-Z(T.FNodePtrs) = U;
-Z(T.NodeFlag==2) = T.Nodes(T.NodeFlag==2, 2);
-Z(T.NodeFlag==3) = g(T.Nodes(T.NodeFlag==3 ,1));
-Z(T.NodeFlag==4) = T.Nodes(T.NodeFlag==4, 2);
+Z(T.P2.FNodePtrs) = U;
+Z(T.P2.NodeFlag==2) = T.P2.Nodes(T.P2.NodeFlag==2, 2);
+Z(T.P2.NodeFlag==3) = g(T.P2.Nodes(T.P2.NodeFlag==3 ,1));
+Z(T.P2.NodeFlag==4) = T.P2.Nodes(T.P2.NodeFlag==4, 2);
 
-figure(2); trisurf(T.TP, T.Nodes(:,1), T.Nodes(:,2), Z);
+figure(2); trisurf(T.P2.TP, T.P2.Nodes(:,1), T.P2.Nodes(:,2), Z);
 
