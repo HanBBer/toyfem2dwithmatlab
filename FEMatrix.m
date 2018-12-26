a4bfb24be0230e19bf3ff5f3cd2c05a4bcf355e6
Han <hanyin15@163.com>
2018-12-26 14:57:54 +0800

    Add the indicator for mass matrix

--- FEMatrix.m
+++ FEMatrix.m
@@ -1,6 +1,6 @@
 function K = FEMatrix(T, Fd1, d1, Fd2, d2)
 U1 = Fd1.Space;
-if nargin < 5; U2 = U1; Fd2 = Fd1; d2 = d1; end
+if nargin < 4; U2 = U1; Fd2 = Fd1; d2 = d1; end
 [w, P, Px, Py, C01, C02] = LoadQuad();
 if (T.(U1).Property == "P1");   C1 = C01;
 else;  C1 = C02; end
@@ -24,25 +24,25 @@ for i = 1:T.Nt
             for r = 1:nr
                 if Fd2.NodeFlag(j2(r)) == 0
                     c2 = C2(:, r);
-                    if ~isempty(d1)
+                    if d1 == "mass"
+                        I1 = P*c1;
+                    else
                         V1 = [Px*c1 Py*c1];
                         switch d1
                             case {"dx"}; I1 = V1*J(:,1);
                             case {"dy"}; I1 = V1*J(:,2);
                             case {"nabla"}; I1 = V1(:, 1:2)/A;
                         end
-                    else
-                        I1 = P*c1;
                     end
-                    if ~isempty(d2)
+                    if d2 == "mass"
+                        I2 = P*c2;
+                    else
                         V2 = [Px*c2 Py*c2];
                         switch d2
                             case {"dx"}; I2 = V2*J(:,1);
                             case {"dy"}; I2 = V2*J(:,2);
                             case {"nabla"}; I2 = V2(:, 1:2)/A;
                         end
-                    else
-                        I2 = P*c2;
                     end
 
                     I = sum(w*(I1.*I2)/2);
