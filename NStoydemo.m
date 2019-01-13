3d5e6ce5b02c230b631934055d6209ad6ea592d6
Han <hanyin15@163.com>
2019-01-13 23:12:13 +0800

    Add the effct of boundary condtion in the last time step

--- NStoydemo.m
+++ NStoydemo.m
@@ -20,8 +20,8 @@ dt = 0.02; nu = 0.7; t = 0; t1 = 10;
 
 f1 = @(x,y) sin(x).*cos(y+t)+(2*nu-1)*sin(x).*sin(y+t);
 f2 = @(x,y) -cos(x).*sin(y+t)+(2*nu+1)*cos(x).*cos(y+t);
-u1 = @(x,y) sin(x).*sin(y+t);
-u2 = @(x,y) cos(x).*cos(y+t);
+U1 = @(x,y) sin(x).*sin(y+t);
+U2 = @(x,y) cos(x).*cos(y+t);
 
 T = RecMesh(n, n, 1, 1, 0, 0);
 
@@ -48,65 +48,72 @@ ucord = T.U.Nodes(Fdu.FNodePtrs,:);
 uccord = T.U.Nodes(Fdu.CNodePtrs,:);
 pcord = T.P.Nodes(Fdp.FNodePtrs,:);
 
-u10 = u1(ucord(:,1), ucord(:,2));
-u20 = u2(ucord(:,1), ucord(:,2));
+u10 = U1(ucord(:,1), ucord(:,2));
+u20 = U2(ucord(:,1), ucord(:,2));
 p0 = zeros(Nfp,1);
+G10 = {U1, U1, U1, []};
+G20 = {U2, U2, U2, []};
 
 for t = dt:dt:t1
     f1 = @(x,y) sin(x).*cos(y+t)+(2*nu-1)*sin(x).*sin(y+t);
     f2 = @(x,y) -cos(x).*sin(y+t)+(2*nu+1)*cos(x).*cos(y+t);
     
     %Truth
-    u1 = @(x,y) sin(x).*sin(y+t); 
-    u2 = @(x,y) cos(x).*cos(y+t);
+    U1 = @(x,y) sin(x).*sin(y+t); 
+    U2 = @(x,y) cos(x).*cos(y+t);
     %pe = @(x,y) cos(x).*sin(y+t);
     
-    G1 = {u1, u1, u1, []};
-    G2 = {u2, u2, u2, []};
+    G1 = {U1, U1, U1, []};
+    G2 = {U2, U2, U2, []};
 
     BigF = zeros(2*Nfu+Nfp,1);
-    BigF(ind1:ind2-1) = FemBiLoad(T, Fdu, 'mass', G1)/dt + Mu/dt*u10 + nu*FemBiLoad(T, Fdu, 'nabla', G1)  + FemLinearLoad(T, Fdu, f1, []);
-    BigF(ind2:ind3-1) = FemBiLoad(T, Fdu, 'mass', G2)/dt + Mu/dt*u20 + nu*FemBiLoad(T, Fdu, 'nabla', G2)  + FemLinearLoad(T, Fdu, f2, []);
+    BigF(ind1:ind2-1) = FemBiLoad(T, Fdu, 'mass', G1) + Mu/dt*u10 - FemBiLoad(T, Fdu, 'mass', G10)/dt + nu*FemBiLoad(T, Fdu, 'nabla', G1)  + FemLinearLoad(T, Fdu, f1, []);
+    BigF(ind2:ind3-1) = FemBiLoad(T, Fdu, 'mass', G2) + Mu/dt*u20 - FemBiLoad(T, Fdu, 'mass', G20)/dt + nu*FemBiLoad(T, Fdu, 'nabla', G2)  + FemLinearLoad(T, Fdu, f2, []);
     BigF(ind3:end) = -FemBiLoad(T, Fdp, 'mass', G1, Fdu, 'dx') - FemBiLoad(T, Fdp, 'mass', G2, Fdu, 'dy');
  
     U = BigK\BigF;
     %U = bicg(BigK,BigF);
+
+    G10 = G1;
+    G20 = G2;
     u10 = U(ind1:ind2-1);
     u20 = U(ind2:ind3-1);
     p = U(ind3:end);
-    u1ne = u1(ucord(:,1), ucord(:,2));
-    u2ne = u2(ucord(:,1), ucord(:,2));
+    u1ne = U1(ucord(:,1), ucord(:,2));
+    u2ne = U2(ucord(:,1), ucord(:,2));
     fprintf("error of u1:%f\n", norm(u1ne-u10)/sqrt(Nfu));
     
     Z = zeros(size(T.U.Nodes, 1), 1);
     Z(Fdu.FNodePtrs) = u10;
-    Z(Fdu.CNodePtrs) = u1( uccord(:,1), uccord(:,2) );
+    Z(Fdu.CNodePtrs) = U1( uccord(:,1), uccord(:,2) );
     subplot(1,2,1)
     trimesh(T.U.TP, T.U.Nodes(:,1), T.U.Nodes(:,2), Z);
     subplot(1,2,2)
-    trimesh(T.U.TP, T.U.Nodes(:,1), T.U.Nodes(:,2), u1(T.U.Nodes(:,1), T.U.Nodes(:,2)));
+    trimesh(T.U.TP, T.U.Nodes(:,1), T.U.Nodes(:,2), U1(T.U.Nodes(:,1), T.U.Nodes(:,2)));
     pause(0.01)
     
 end
 
 % Validation Part
 t = 0;
-u1 = @(x,y) sin(x).*sin(y+t);
-u2 = @(x,y) cos(x).*cos(y+t);
-pe = @(x,y) cos(x).*sin(y+t);
-u11 = u1(ucord(:,1), ucord(:,2));
-u21 = u2(ucord(:,1), ucord(:,2));
-U1 = [ u11; u21; pe(pcord(:,1), pcord(:,2))];
+U1 = @(x,y) sin(x).*sin(y+t);
+U2 = @(x,y) cos(x).*cos(y+t);
+%pe = @(x,y) cos(x).*sin(y+t);
+G10 = {U1, U1, U1, []};
+G20 = {U2, U2, U2, []};
+u11 = U1(ucord(:,1), ucord(:,2));
+u21 = U2(ucord(:,1), ucord(:,2));
+%U1 = [ u11; u21; pe(pcord(:,1), pcord(:,2))];
 BigF = zeros(2*Nfu+Nfp,1);
-BigF(ind1:ind2-1) = FemBiLoad(T, Fdu, 'mass', G1)/dt + Mu/dt*u11 + nu*FemBiLoad(T, Fdu, 'nabla', G1)  + FemLinearLoad(T, Fdu, f1, []);
-BigF(ind2:ind3-1) = FemBiLoad(T, Fdu, 'mass', G2)/dt + Mu/dt*u21 + nu*FemBiLoad(T, Fdu, 'nabla', G2)  + FemLinearLoad(T, Fdu, f2, []);
+BigF(ind1:ind2-1) = FemBiLoad(T, Fdu, 'mass', G1)/dt + Mu/dt*u11 - FemBiLoad(T, Fdu, 'mass', G10)/dt + nu*FemBiLoad(T, Fdu, 'nabla', G1)  + FemLinearLoad(T, Fdu, f1, []);
+BigF(ind2:ind3-1) = FemBiLoad(T, Fdu, 'mass', G2)/dt + Mu/dt*u21 - FemBiLoad(T, Fdu, 'mass', G20)/dt + nu*FemBiLoad(T, Fdu, 'nabla', G2)  + FemLinearLoad(T, Fdu, f2, []);
 BigF(ind3:end) = - FemBiLoad(T, Fdp, 'mass', G1, Fdu, 'dx') - FemBiLoad(T, Fdp, 'mass', G2, Fdu, 'dy');
 
 t = dt;
-u1 = @(x,y) sin(x).*sin(y+t);
-u2 = @(x,y) cos(x).*cos(y+t);
+U1 = @(x,y) sin(x).*sin(y+t);
+U2 = @(x,y) cos(x).*cos(y+t);
 pe = @(x,y) cos(x).*sin(y+t);
-u12 = u1(ucord(:,1), ucord(:,2));
-u22 = u2(ucord(:,1), ucord(:,2));
+u12 = U1(ucord(:,1), ucord(:,2));
+u22 = U2(ucord(:,1), ucord(:,2));
 U2 = [u12; u22; pe(pcord(:,1), pcord(:,2))];
 fprintf("The Model Error is: %f\n", BigK*U2-BigF);
