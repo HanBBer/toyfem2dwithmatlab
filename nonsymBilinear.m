cd72b0ea06e0fe1e58cb036323ee7e8e7be05ca7
Han <hanyin15@163.com>
2019-02-07 20:16:27 +0800

    Fix bug

--- nonsymBilinear.m
+++ nonsymBilinear.m
@@ -1,4 +1,4 @@
-function K = nonsymBilinear(U1, type1, U2, type2, fnk)
+function K = nonsymBilinear(T, U1, type1, U2, type2, fnk)
 % This function calculates the inner product of finite element function
 % We implements this process element by element
 % Here we do not treat any kind of boundary condition
@@ -10,35 +10,20 @@ function K = nonsymBilinear(U1, type1, U2, type2, fnk)
 if nargin < 5; fnk = []; end
 degree = max(U1.Degree, U2.Degree);
 Quads2d = LoadQuad2d(2*degree);
-if degree == 1
-    P = [ones(size(Quads2d.px)), Quads2d.px, Quads2d.py];
-    Px = [zeros(size(Quads2d.px)), ones(size(Quads2d.px)), zeros(size(Quads2d.px))];
-    Py = [zeros(size(Quads2d.px)), zeros(size(Quads2d.px)), ones(size(Quads2d.px))];
-    rcord = [0, 0; 1, 0; 0, 1];
-    C = [ones(size(rcord,1),1), rcord];
-end
-if degree == 2
-    P = [ones(size(Quads2d.px)), Quads2d.px, Quads2d.py,...
-        Quads2d.px.^2, Quads2d.px.*Quads2d.py, Quads2d.py.^2];
-    Px = [zeros(size(Quads2d.px)), ones(size(Quads2d.px)), zeros(size(Quads2d.px)),...
-        2*Quads2d.px, Quads2d.py, zeros(size(Quads2d.px))];
-    Py = [zeros(size(Quads2d.px)), zeros(size(Quads2d.px)), ones(size(Quads2d.px)),...
-        zeros(size(Quads2d.px)), Quads2d.px, 2*Quads2d.py];
-    rcord = [0, 0; 1, 0; 0, 1; 1/2, 0; 1/2, 1/2; 0, 1/2];
-    C = [ones(size(rcord,1),1), rcord, rcord(:,1).^2, rcord(:,1).*rcord(:,2), rcord(:,2).^2];
-end
+Tr1 = RefInfo(U1.Degree, Quads2d);
+Tr2 = RefInfo(U2.Degree, Quads2d);
 % M = inv(C); M is the coefficients matrix of basis funtions
 if type1 == "mass"
-    Ical1 = P/C;
+    Ical1 = Tr1.P/Tr1.C;
 else
-    Ixcal1 = Px/C;
-    Iycal1 = Py/C;
+    Ixcal1 = Tr1.Px/Tr1.C;
+    Iycal1 = Tr1.Py/Tr1.C;
 end
 if type2 == "mass"
-    Ical2 = P/C;
+    Ical2 = Tr2.P/Tr2.C;
 else
-    Ixcal2 = Px/C;
-    Iycal2 = Py/C;
+    Ixcal2 = Tr2.Px/Tr2.C;
+    Iycal2 = Tr2.Py/Tr2.C;
 end
 indk = 1;
 n1 = size(U1.TC(1, :), 2);
@@ -47,16 +32,18 @@ tmp = ceil(U1.Nt*n1*n2);
 indi = ones(tmp ,1);
 indj = ones(tmp, 1);
 valuek = zeros(tmp, 1);
-for i = 1:U1.Nt
-    ind = U1.TC(i, :);
-    cord = U1.Node(ind, :);
+for i = 1:T.Nt
+    ind = T.Tri(i, :);
+    cord = T.Node(ind, :);
     A = (cord(2:3, :) - repmat(cord(1, :),2,1))';
     Area = abs(det(A));
     J = inv(A);
+    ind1 = U1.TC(i, :);
+    ind2 = U2.TC(i, :);
     for s = 1:n1
         for r = 1:n2
-            indi(indk, 1) = ind(s);
-            indj(indk, 1) = ind(r);
+            indi(indk, 1) = ind1(s);
+            indj(indk, 1) = ind2(r);
             switch type1
                 case {"mass"}; I1 = Ical1(:, s);
                 case {"dx"}; I1 = ([Ixcal1(:, s), Iycal1(:, s)]*J(:, 1));
@@ -64,7 +51,7 @@ for i = 1:U1.Nt
                 case {"nabla"}; I1 = ([Ixcal1(:, s), Iycal1(:, s)]/A);
             end
             switch type2
-                case {"mass"}; I2 = Ical2(:, s);
+                case {"mass"}; I2 = Ical2(:, r);
                 case {"dx"}; I2 = ([Ixcal2(:, r), Iycal2(:, r)]*J(:, 1));
                 case {"dy"}; I2 = ([Ixcal2(:, r), Iycal2(:, r)]*J(:, 2));
                 case {"nabla"}; I2 = ([Ixcal2(:, r), Iycal2(:, r)]/A);
