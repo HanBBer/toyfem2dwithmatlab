6a8129d4818649c20d9c19b017391016862f2656
Han <hanyin15@163.com>
2019-02-07 15:19:00 +0800

    Define (f, v)

---
+++ Load.m
@@ -0,0 +1,37 @@
+function F = Load(U, f)
+% This function assembles the Load Vector
+if nargin < 2; f = @(x, y) 1; end
+degree = U.Degree;
+Quads2d = LoadQuad2d(2*degree);
+if degree == 1
+    P = [ones(size(Quads2d.px)), Quads2d.px, Quads2d.py];
+    rcord = [0, 0; 1, 0; 0, 1];
+    C = [ones(size(rcord,1), 1), rcord];
+end
+if degree == 2
+    P = [ones(size(Quads2d.px)), Quads2d.px, Quads2d.py,...
+        Quads2d.px.^2, Quads2d.px.*Quads2d.py, Quads2d.py.^2];
+    rcord = [0, 0; 1, 0; 0, 1; 1/2, 0; 1/2, 1/2; 0, 1/2];
+    C = [ones(size(rcord,1), 1), rcord, rcord(:,1).^2, rcord(:,1).*rcord(:,2), rcord(:,2).^2];
+end
+Ical = P/C;
+n = size(U.TC(1, :), 2);
+N = U.Nt*n;
+indi = ones(N, 1);
+indj = ones(N, 1);
+valueF = zeros(N, 1);
+
+for i = 1:U.Nt
+    ind = U.TC(i, :);
+    cord = U.Node(ind, :);
+    A = (cord(2:3, :) - repmat(cord(1, :),2,1))';
+    Area = abs(det(A));
+    for s = 1:n
+        pos = (i-1)*n+s;
+        cordf = repmat(cord(1, :), size(Quads2d.px, 1), 1) + [Quads2d.px, Quads2d.py]*A';
+        indi(pos) = ind(s);
+        valueF(pos) = Quads2d.w*(f(cordf(:,1), cordf(:,2)).*Ical(:, s))/2*Area;
+    end
+end
+F = sparse(indi, indj, valueF);
+end
