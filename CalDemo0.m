e872f813e5030a03c04ae4ecd29e7dad6e26c844
Han <hanyin15@163.com>
2019-03-08 10:40:56 +0800

    Add test for 1d intergral

--- CalDemo0.m
+++ CalDemo0.m
@@ -1,10 +1,49 @@
 % Use fem calculate 2d integral
 % To simplify the program, we do the integrals on the same domain
 
+%% 0. Quadrature test
+clear
+U1.Degree = 1;
+U1.N = 4;
+U1.Nt = 1;
+U1.Node = [0,1; 1,0; 0,0];
+U1.TC = [1,2,3];
+U1.Tri = [1,2,3];
+K1 = symBilinear(U1, 'mass');
+u = @(x, y) x;
+v = @(x, y) y;
+
+% Get Nodal info
+U = u(U1.Node(:,1), U1.Node(:,2));
+V = v(U1.Node(:,1), U1.Node(:,2));
+
+fprintf("0\n")
+fprintf("The FEM approximation value is %.6f\n", U'*K1*V);
+fprintf("The true value is %.6f\n", integral2(@(x,y) x.*y,0,1,0,@(x)1-x));
+
+U2.Degree = 2;
+U2.N = 3;
+U2.Nt = 1;
+U2.Node = [0,1; 0,0; 1,0; 0, 1/2; 1/2, 0; 1/2, 1/2];
+U2.TC = [1,2,3,4,5,6];
+U2.Tri = [1,4,6;4,2,5;4,5,6;6,5,3];
+K2 = symBilinear(U2, 'mass');
+u = @(x, y) x.^2;
+v = @(x, y) x.*y;
+
+%% Intergration Part
+% Get Nodal info
+U = u(U2.Node(:,1), U2.Node(:,2));
+V = v(U2.Node(:,1), U2.Node(:,2));
+
+fprintf("The FEM approximation value is %.6f\n", U'*K2*V);
+fprintf("The true value is %.6f\n", integral2(@(x,y) x.^3.*y,0,1,0,@(x)1-x));
+
 % Define the domain and generate mesh
+clear
 N = 4;
 Nx = N; Ny = N;
-T = RecMesh([Nx, Ny], [1,1], [0,0]);
+T = RecMesh([Nx, Ny], [1, 1], [0, 0]);
 
 % Define a space for calculation
 P = P1Fespace(T);
@@ -74,39 +113,42 @@ fprintf("4\n")
 fprintf("The FEM approximation value is %.6f\n", U1'*Kuxp*V1);
 fprintf("The true value is %.6f\n", integral2(@(x,y) 2*x.*y,0,1,0,1));
 
-%% 0. Quadrature test
-clear
-U1.Degree = 1;
-U1.N = 3;
-U1.Nt = 1;
-U1.Node = [0,1; 1,0; 0,0];
-U1.TC = [1,2,3];
-U1.Tri = [1,2,3];
-K1 = symBilinear(U1, 'mass');
-u = @(x, y) x;
+%% 5.Test of numP2Bilinear
+u = @(x, y) x.^2;
+f = @(x, y) x;
 v = @(x, y) y;
 
-% Get Nodal info
-U = u(U1.Node(:,1), U1.Node(:,2));
-V = v(U1.Node(:,1), U1.Node(:,2));
+fnk = f(U.Node(:,1), U.Node(:,2));
+Mat1 = numP2Bilinear(U, fnk, "dx", "mass");
+Mat2 = numP2Bilinear(U, fnk, "mass", "dx");
+U1 = u(U.Node(:,1), U.Node(:,2));
+V1 = v(U.Node(:,1), U.Node(:,2));
+fprintf("5\n")
+fprintf("The FEM approximation value is %.6f\n", V1'*Mat1*U1);
+fprintf("The true value is %.6f\n", integral2(@(x,y) 2*x.^2.*y,0,1,0,1));
+fprintf("The FEM approximation value is %.6f\n", V1'*Mat2*U1);
+fprintf("The true value is %.6f\n", integral2(@(x,y) x.^2.*y,0,1,0,1));
 
-fprintf("0\n")
-fprintf("The FEM approximation value is %.6f\n", U'*K1*V);
-fprintf("The true value is %.6f\n", integral2(@(x,y) x.*y,0,1,0,@(x)1-x));
 
-U2.Degree = 2;
-U2.N = 3;
-U2.Nt = 1;
-U2.Node = [0,1; 0,0; 1,0; 0, 1/2; 1/2, 0; 1/2, 1/2];
-U2.TC = [1,2,3,4,5,6];
-U2.Tri = [1,4,6;4,2,5;4,5,6;6,5,3];
-K2 = symBilinear(U2, 'mass');
-u = @(x, y) x.^2;
-v = @(x, y) x.*y;
+%% 6. Test of Bilinear1d
+u = @(x, y) x.^2.*y;
+v = @(x, y) y;
 
-% Get Nodal info
-U = u(U2.Node(:,1), U2.Node(:,2));
-V = v(U2.Node(:,1), U2.Node(:,2));
+U1 = u(P.Node(:, 1), P.Node(:, 2));
+V1 = v(P.Node(:, 1), P.Node(:, 2));
+U2 = u(U.Node(:, 1), U.Node(:, 2));
+V2 = v(U.Node(:, 1), U.Node(:, 2));
 
-fprintf("The FEM approximation value is %.6f\n", U'*K2*V);
-fprintf("The true value is %.6f\n", integral2(@(x,y) x.^3.*y,0,1,0,@(x)1-x));
+M1 = Bilinear1d(T, 3, P, "mass", P, "mass");
+M2 = Bilinear1d(T, 3, U, "mass", U, "mass");
+Mdx1 = Bilinear1d(T, 3, P, "dx", P, "mass");
+Mdx2 = Bilinear1d(T, 3, U, "dx", U, "mass");
+fprintf("6\n");
+fprintf("The FEM approximation value is %.6f\n", V1'*M1*U1);
+fprintf("The FEM approximation value is %.6f\n", U1'*M1*V1);
+fprintf("The FEM approximation value is %.6f\n", U2'*M2*V2);
+fprintf("The true value is %.6f\n", integral(@(x) x.^2, 0,1));
+fprintf("The FEM approximation value is %.6f\n", U1'*Mdx1*V1);
+fprintf("The FEM approximation value is %.6f\n", U2'*Mdx2*V2);
+fprintf("The true value is %.6f\n", integral(@(x) 2*x, 0,1));
+
